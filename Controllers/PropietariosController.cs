1fc4637bc45b54a8c830c66e36dcf71abc16da0a
Santiago8333 <91435566+Santiago8333@users.noreply.github.com>
2024-08-22 00:04:51 -0300

    v0.0.2

--- Controllers/PropietariosController.cs
+++ Controllers/PropietariosController.cs
@@ -21,4 +21,43 @@ public class PropietariosController : Controller
         return View(lista);
     }
 
+[HttpPost]
+public IActionResult Agregar(Propietario nuevoPropietario)
+{
+    if (ModelState.IsValid)
+    {
+        // Aquí agregas el nuevo propietario a la base de datos
+        repo.AgregarPropietario(nuevoPropietario);
+
+        // Rediriges de nuevo al Index después de agregar
+        return RedirectToAction("Index");
+    }
+
+    // Si hay un error, recargas la lista de propietarios y vuelves a la vista Index
+    var lista = repo.ObtenerTodos();
+    return View("Index", lista);
+}
+
+public IActionResult Eliminar(int id)
+{
+    try
+    {
+        repo.EliminarPropietario(id);
+        return RedirectToAction("Index");
+    }
+    catch (Exception ex)
+    {
+        
+        return View("Error", new { message = ex.Message });
+    }
+}
+/*
+public IActionResult Edicion(int id)
+{
+if(id == 0)
+    return View();
+else
+    
+}
+*/
 }
--- Models/RepositorioPropietario.cs
+++ Models/RepositorioPropietario.cs
@@ -34,4 +34,41 @@ public class RepositorioPropietario
 			return propietarios;
 		}
 	}
+		public void AgregarPropietario(Propietario nuevoPropietario)
+{
+	
+    using(MySqlConnection connection = new MySqlConnection(ConectionString))
+    {
+        var query = $@"INSERT INTO propietarios ({nameof(Propietario.Dni)}, {nameof(Propietario.Apellido)}, {nameof(Propietario.Nombre)}, {nameof(Propietario.Email)}, {nameof(Propietario.Telefono)})
+                    VALUES (@Dni, @Apellido, @Nombre,@Email,@Telefono)";
+                       
+        using(MySqlCommand command = new MySqlCommand(query, connection))
+        {
+            // Agrega los parámetros
+			command.Parameters.AddWithValue("@Dni", nuevoPropietario.Dni);
+            command.Parameters.AddWithValue("@Apellido", nuevoPropietario.Apellido);
+            command.Parameters.AddWithValue("@Nombre", nuevoPropietario.Nombre);
+            command.Parameters.AddWithValue("@Email", nuevoPropietario.Email);
+			command.Parameters.AddWithValue("@Telefono", nuevoPropietario.Telefono);
+
+            connection.Open();
+            command.ExecuteNonQuery(); // Ejecuta la consulta de inserción
+            connection.Close();
+        }
+    }
+}
+		public void EliminarPropietario(int id)
+{
+using (MySqlConnection connection = new MySqlConnection(ConectionString))
+    {
+		var query = "DELETE FROM propietarios WHERE Id_propietarios = @Id";
+ 		using(MySqlCommand command = new MySqlCommand(query, connection))
+        {
+			 command.Parameters.AddWithValue("@Id", id);
+            connection.Open();
+            command.ExecuteNonQuery(); // Ejecuta la consulta de inserción
+            connection.Close();
+        }
+	}
+}
 }
