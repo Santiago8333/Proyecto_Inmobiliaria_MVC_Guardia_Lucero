fe7d9bb52830779682e29c89440dcf76f8a7ae2a
Santiago8333 <91435566+Santiago8333@users.noreply.github.com>
2024-11-21 20:10:11 -0300

    v0.4.8

--- proyecto_inmobiliaria_mvc_guardia_lucero.sql
+++ proyecto_inmobiliaria_mvc_guardia_lucero.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-09-2024 a las 22:14:37
+-- Tiempo de generación: 22-11-2024 a las 00:09:14
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -32,21 +32,26 @@ CREATE TABLE `contrato` (
   `Id_inquilino` int(11) DEFAULT NULL,
   `Id_inmueble` int(11) DEFAULT NULL,
   `Monto` decimal(10,2) DEFAULT NULL,
+  `Monto_total` decimal(10,2) NOT NULL,
   `Fecha_desde` date DEFAULT NULL,
   `Fecha_hasta` date DEFAULT NULL,
+  `FechaTerminacion` date NOT NULL,
   `Monto_Pagar` decimal(10,2) NOT NULL,
-  `Estado` varchar(50) DEFAULT NULL
+  `Meses` int(11) NOT NULL,
+  `Estado` tinyint(1) DEFAULT NULL,
+  `Contrato_Completado` tinyint(1) NOT NULL,
+  `Create_user` varchar(255) NOT NULL,
+  `Terminate_user` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `contrato`
 --
 
-INSERT INTO `contrato` (`Id_contrato`, `Id_inquilino`, `Id_inmueble`, `Monto`, `Fecha_desde`, `Fecha_hasta`, `Monto_Pagar`, `Estado`) VALUES
-(1, 6, 6, '50000.00', '2024-09-06', '2024-10-01', '32330.00', '1'),
-(3, 1, 1, '6900.00', '2024-09-05', '2024-10-12', '2600.00', '1'),
-(4, 4, 2, '17000.00', '2024-09-05', '2024-11-09', '15500.00', '1'),
-(10, 1, 1, '5000.00', '2024-11-01', '2024-12-07', '5000.00', '1');
+INSERT INTO `contrato` (`Id_contrato`, `Id_inquilino`, `Id_inmueble`, `Monto`, `Monto_total`, `Fecha_desde`, `Fecha_hasta`, `FechaTerminacion`, `Monto_Pagar`, `Meses`, `Estado`, `Contrato_Completado`, `Create_user`, `Terminate_user`) VALUES
+(15, 3, 3, '350000.00', '2100000.00', '2024-10-01', '2025-01-30', '2024-10-08', '1750000.00', 6, 0, 0, 'admin@admin', 'admin@admin'),
+(16, 1, 4, '400000.00', '400000.00', '2024-10-01', '2024-10-30', '2024-10-30', '0.00', 1, 1, 1, 'admin@admin', ''),
+(23, 1, 6, '200000.00', '400000.00', '2024-10-01', '2024-11-30', '2024-11-30', '200000.00', 2, 1, 0, 'admin@admin', '');
 
 -- --------------------------------------------------------
 
@@ -71,12 +76,12 @@ CREATE TABLE `inmuebles` (
 --
 
 INSERT INTO `inmuebles` (`Id_inmueble`, `Id_propietario`, `Uso`, `Tipo`, `Ambiente`, `Precio`, `Direccion`, `Cordenada`, `Estado`) VALUES
-(1, 8, 'Comercial', 'depósito', '6', '1900.00', 'La punta ,modulo 1 ', '-21344 -21344234', 1),
-(2, 11, 'Residencial', 'departamento', '2', '1457.00', 'La Punta,modulo 2', '-38.98800246677288, -61.28767380446236', 1),
-(3, 2, 'Residencial', 'casa', '2', '1457.00', 'La Punta,modulo 3', '-21344 -21344234', 1),
-(4, 10, 'Residencial', 'casa', '6', '1457.00', 'La Punta,modulo 4', '-21344 -21344234', 1),
-(5, 11, 'Residencial', 'depósito', '6', '1457.00', 'La Punta,modulo 5', '-38.98800246677288, -61.28767380446236', 1),
-(6, 2, 'Comercial', 'depósito', '7', '349999.00', 'San Luis,', '-38.98800246677288, -61.28767380446236', 1);
+(1, 8, 'Comercial', 'depósito', '3', '300000.00', 'La Punta ,modulo 1, Manzana 108, casa 10', '33°11\'02\"S 66°18\'26\"W', 1),
+(2, 11, 'Residencial', 'departamento', '3', '180000.00', 'La Punta, Modulo 7, Manzana 17, casa 2', '33°10\'58\"S 66°19\'11\"W', 1),
+(3, 2, 'Residencial', 'casa', '5', '350000.00', 'La Punta, Modulo 3, Manzana 8, casa 18', '33°10\'53\"S 66°19\'00\"W', 1),
+(4, 10, 'Residencial', 'casa', '6', '400000.00', 'La Punta,modulo 4, Manzana 15, casa 2', '33°11\'18\"S 66°18\'56\"W', 1),
+(5, 11, 'Residencial', 'depósito', '4', '350000.00', 'La Punta,modulo 8, Manzana 22, casa 4', '33°10\'38\"S 66°19\'02\"W', 1),
+(6, 2, 'Comercial', 'depósito', '4', '200000.00', 'La Punta, Los Lapachos, Manzana 10, casa 20', '33°09\'52\"S 66°18\'59\"W', 0);
 
 -- --------------------------------------------------------
 
@@ -99,12 +104,33 @@ CREATE TABLE `inquilinos` (
 --
 
 INSERT INTO `inquilinos` (`Id_inquilinos`, `Dni`, `Apellido`, `Nombre`, `Email`, `Telefono`, `Estado`) VALUES
-(1, '5555', 'test', 'test', 'test@test', '266544', 1),
-(2, '9239423', 'Kilo', 'Lopexz', 'test2@test', '26645', 1),
-(3, '4366634', 'Kilo', 'Jose', 'test@jose', '26645', 1),
-(4, '4366634', 'Doe', 'Jon', 'test@example.us', '6019521325', 1),
-(5, '78838', 'García Flores', 'Juan Francisco', 'ejemplo@ejemplo.mx', '5553428400', 1),
-(6, '89012222', 'Doe', 'Jon', 'test@fr.us', '6019521325', 1);
+(1, '26566324', 'Toledo', 'Martin', 'martintoledo@gmail.com', '2665445425', 1),
+(2, '33555221', 'Maure', 'Javier', 'javiermaure@gmail.com', '2664545245', 1),
+(3, '43666344', 'Lujan', 'Paulo', 'paulolujan@gmail.com', '26645524521', 1),
+(4, '43223441', 'Diaz', 'Manuel', 'manueldiaz@gmail.com', '2664221133', 1),
+(5, '31452211', 'García Flores', 'Juan Francisco', 'juangarcia@gmail.com', '2665342840', 1),
+(6, '29012222', 'Dominguez', 'Jonathan', 'jonathandominguez@gmail.com', '2664195213', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `multa`
+--
+
+CREATE TABLE `multa` (
+  `Id_multa` int(11) NOT NULL,
+  `Id_contrato` int(11) NOT NULL,
+  `Monto` decimal(10,2) NOT NULL,
+  `RazonMulta` varchar(500) NOT NULL,
+  `Fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `multa`
+--
+
+INSERT INTO `multa` (`Id_multa`, `Id_contrato`, `Monto`, `RazonMulta`, `Fecha`) VALUES
+(1, 15, '700000.00', 'Multa de 2 Meses,No se Cumplio menos de la mitad del tiempo de la duracion del contrato', '2024-10-08');
 
 -- --------------------------------------------------------
 
@@ -115,8 +141,11 @@ INSERT INTO `inquilinos` (`Id_inquilinos`, `Dni`, `Apellido`, `Nombre`, `Email`,
 CREATE TABLE `pago` (
   `Id_pago` int(11) NOT NULL,
   `Id_contrato` int(11) DEFAULT NULL,
+  `Detalle` varchar(255) NOT NULL,
   `Fecha_pago` date DEFAULT NULL,
   `Monto` decimal(10,2) DEFAULT NULL,
+  `Create_user` varchar(255) NOT NULL,
+  `Anulado_user` varchar(255) NOT NULL,
   `Estado` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -124,19 +153,16 @@ CREATE TABLE `pago` (
 -- Volcado de datos para la tabla `pago`
 --
 
-INSERT INTO `pago` (`Id_pago`, `Id_contrato`, `Fecha_pago`, `Monto`, `Estado`) VALUES
-(1, 1, '2024-09-07', '500.00', 1),
-(2, 1, '2024-09-08', '500.00', 1),
-(3, 1, '2024-09-10', '7000.00', 1),
-(4, 1, '2024-09-14', '9000.00', 1),
-(5, 1, '2024-09-18', '570.00', 1),
-(6, 1, '2024-09-28', '100.00', 1),
-(7, 3, '2024-09-09', '500.00', 0),
-(8, 3, '2024-09-12', '5000.00', 0),
-(9, 4, '2024-09-14', '1500.00', 1),
-(10, 3, '2024-09-11', '700.00', 0),
-(11, 3, '2024-09-08', '1300.00', 1),
-(12, 3, '2024-09-30', '3000.00', 1);
+INSERT INTO `pago` (`Id_pago`, `Id_contrato`, `Detalle`, `Fecha_pago`, `Monto`, `Create_user`, `Anulado_user`, `Estado`) VALUES
+(22, 15, 'Cuota octubre 2024', '2024-10-06', '350000.00', 'admin@admin', 'admin@admin', 0),
+(23, 15, 'Cuota octubre 2024', '2024-10-01', '1400000.00', 'admin@admin', 'admin@admin', 0),
+(24, 15, 'Cuota octubre 2024', '2024-10-01', '350000.00', 'admin@admin', '', 1),
+(25, 15, 'Cuota noviembre 2024', '2024-11-01', '350000.00', 'admin@admin', 'admin@admin', 0),
+(26, 16, 'Cuota octubre 2024', '2024-10-01', '400000.00', 'admin@admin', '', 1),
+(27, 15, 'Cuota noviembre 2024', '2024-11-01', '350000.00', 'admin@admin', '', 1),
+(29, 23, 'Cuota octubre 2024', '2024-10-01', '200000.00', 'admin@admin', '', 1),
+(30, 15, 'Cuota diciembre 2024', '2024-12-01', '350000.00', 'admin@admin', '', 1),
+(31, 15, 'Cuota diciembre 2024', '2024-12-01', '350000.00', 'admin@admin', 'admin@admin', 0);
 
 -- --------------------------------------------------------
 
@@ -159,15 +185,15 @@ CREATE TABLE `propietarios` (
 --
 
 INSERT INTO `propietarios` (`Id_propietarios`, `Dni`, `Apellido`, `Nombre`, `Email`, `Telefono`, `Estado`) VALUES
-(1, '53242333', 'Lopez', 'Juan', 'juan@juan', '277891222', 1),
-(2, '3912234', 'test100', 'Lopexz', 'kilo@lopez.com', '266999', 1),
-(5, '991111', 'Pepe', 'jose', 'test@jose', '324823', 1),
+(1, '32242333', 'Lopez', 'Juan', 'juan@gmail.com', '2664891222', 1),
+(2, '28122345', 'Laborda', 'Leonardo', 'leolaborda@gmail.com', '2665214587', 1),
+(5, '43421865', 'Cuello', 'Marcos', 'marcoscuello@gmail.com', '2664897452', 1),
 (6, '90122', 'test2', 'Jose', 'test2@test456', '77777', 0),
-(7, '4366634', 'test', 'tset', 'test2@test', '3255555', 1),
-(8, '45359334', 'Pepe', 'jose', 'pepe@pepe300', '77778', 1),
-(9, 'tooltip', 'tooltip', 'tooltip', 'sa@da', '213443', 1),
-(10, '82833', 'test', 'test', 'test@test', '90012', 1),
-(11, '642333', 'Kilo', 'test2', 'pepe@pepe', '2663992222234', 1),
+(7, '4366634', 'Figueroa', 'Analia', 'analiafigueroa@gmail.com', '2664512871', 1),
+(8, '45333654', 'Camargo', 'Julia', 'julicamargo@gmail.com', '2665873214', 1),
+(9, '33564875', 'Miranda', 'Luis', 'luismiranda@gmail.com', '2664212233', 1),
+(10, '41258741', 'Molina', 'Maria Jose', 'Mariajose@gmail.com', '2665988985', 1),
+(11, '22444562', 'Gauna', 'Dario', 'dariogauna@gmail.com', '2664996635', 1),
 (12, '8919222', 'test', 'test', 'test30@test', '2663992222', 0),
 (13, '90000', 'test', 'test', 'test@jose', '2653244344', 0);
 
@@ -194,10 +220,10 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`Id_usuario`, `Nombre`, `Apellido`, `Email`, `Clave`, `AvatarUrl`, `Rol`, `RolNombre`, `Estado`) VALUES
-(1, 'Test ', 'test ', 'test@test', 'Hh5PJIf2spPKILQESYS5hbDBI9MT0YpCqENeX/PwcPE=', '/avatars/14d174a4-5a0d-4a64-a9e7-1ec4fe134ece.png', 2, 'Empleado', 1),
-(2, 'admin', 'adminA', 'admin@admin', 'PJaQVsoZLo39CH/BQi6SoJ2mu8gD6tQfGAvyButelqg=', '/avatars/default-avatar.png', 1, 'Administrador', 1),
+(1, 'Agustin', 'Toledo', 'agustintoledo@gmail.com', 'Hh5PJIf2spPKILQESYS5hbDBI9MT0YpCqENeX/PwcPE=', '/avatars/14d174a4-5a0d-4a64-a9e7-1ec4fe134ece.png', 2, 'Empleado', 1),
+(2, 'admin', 'admin', 'admin@admin', 'FCJBESHoKrpHio8hd8wQ49QdhNSxJHKgkJs9mHAT+6A=', '/avatars/5729b856-a93e-4797-83a9-499edf3fadcd.png', 1, 'Administrador', 1),
 (3, 'jose', 'Pepe', 'test@jose', 'Hh5PJIf2spPKILQESYS5hbDBI9MT0YpCqENeX/PwcPE=', '/avatars/default-avatar.png', 2, 'Empleado', 1),
-(13, 'we', 'aweawe', 'admin@admindawd', 'Hh5PJIf2spPKILQESYS5hbDBI9MT0YpCqENeX/PwcPE=', '/avatars/7319eb8a-4011-4283-9b5b-e2d03c75636c.png', 2, 'Empleado', 1),
+(13, 'Mateo', 'Mateo', 'Mateo@Mateo', 'Hh5PJIf2spPKILQESYS5hbDBI9MT0YpCqENeX/PwcPE=', '/avatars/0a4084ac-16ee-474c-bcac-00c1b007bd8f.png', 2, 'Empleado', 1),
 (14, 'Lopexz', 'Pepe', 'test@test222', 'Hh5PJIf2spPKILQESYS5hbDBI9MT0YpCqENeX/PwcPE=', '/avatars/default-avatar.png', 2, 'Empleado', 1);
 
 --
@@ -224,6 +250,13 @@ ALTER TABLE `inmuebles`
 --
 ALTER TABLE `inquilinos`
   ADD PRIMARY KEY (`Id_inquilinos`);
+
+--
+-- Indices de la tabla `multa`
+--
+ALTER TABLE `multa`
+  ADD PRIMARY KEY (`Id_multa`),
+  ADD KEY `fk_contrato` (`Id_contrato`);
 
 --
 -- Indices de la tabla `pago`
@@ -253,7 +286,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `contrato`
 --
 ALTER TABLE `contrato`
-  MODIFY `Id_contrato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `Id_contrato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT de la tabla `inmuebles`
@@ -268,10 +301,16 @@ ALTER TABLE `inquilinos`
   MODIFY `Id_inquilinos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT de la tabla `multa`
+--
+ALTER TABLE `multa`
+  MODIFY `Id_multa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT de la tabla `pago`
 --
 ALTER TABLE `pago`
-  MODIFY `Id_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `Id_pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT de la tabla `propietarios`
@@ -301,6 +340,12 @@ ALTER TABLE `contrato`
 --
 ALTER TABLE `inmuebles`
   ADD CONSTRAINT `fk_propietario` FOREIGN KEY (`Id_propietario`) REFERENCES `propietarios` (`Id_propietarios`);
+
+--
+-- Filtros para la tabla `multa`
+--
+ALTER TABLE `multa`
+  ADD CONSTRAINT `fk_contrato` FOREIGN KEY (`Id_contrato`) REFERENCES `contrato` (`Id_contrato`) ON DELETE CASCADE;
 
 --
 -- Filtros para la tabla `pago`
